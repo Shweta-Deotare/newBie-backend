63c105d0f064197a6c838ae2363347fc5afcf88b
Shweta-Deotare <shwetadeotare0609@gmail.com>
2023-04-07 12:15:52 +0530

    authentication and user details apis

--- models/account.js
+++ models/account.js
@@ -1,38 +1,65 @@
+const bcrypt = require('../utils/hashPassword');
+
 
 module.exports = {
-  userAccountSchema:(sequelize, DataTypes) => {
-      console.log("user_account Table connected")
-      return sequelize.define('user_accounts', {
-          f_name:{
-              type:DataTypes.STRING,
-              allowNull:false},
-          
-          l_name: {
-              type:DataTypes.STRING,
-              allowNull:false},
-          username: {
-              type:DataTypes.STRING,
-              allowNull:false,
-              primaryKey:true,
-              unique:true},
-          email:{
-              type:DataTypes.STRING,
-              allowNull:false,
-              unique:false},
-          mobile:{
-              type:DataTypes.STRING,
-              allowNull:false,
-              unique:false,
-              validate:{
-                  len:[10,10]},
-              }, 
-          password: {
-              type:DataTypes.STRING, // TO DO : update as hash
-              allowNull:false,
-          },
-      });
-  }    
+    userAccountSchema:(sequelize, DataTypes) => {
+        console.log("user_accounts Table connected")
+        return sequelize.define('user_accounts', {
+            id:{
+                type:DataTypes.INTEGER,
+                allowNull:false,
+                unique: true
+            },
+
+            f_name:{
+                type:DataTypes.STRING,
+                allowNull:false},
+            
+            l_name: {
+                type:DataTypes.STRING,
+                allowNull:false},
+            username: {
+                type:DataTypes.STRING,
+                allowNull:false,
+                primaryKey:true,
+                unique:true},
+            email:{
+                type:DataTypes.STRING,
+                allowNull:false,
+                unique:false},
+            mobile:{
+                type:DataTypes.STRING,
+                allowNull:false,
+                unique:false,
+                validate:{
+                    len:[10,10]},
+                }, 
+            password: {
+                type:DataTypes.STRING, // TO DO : update as hash
+                allowNull:false,
+                validate:{
+                    verifypasswd(value){
+                        if(!(value.length >= 8)){
+                           throw new Error("Password length should be at least 8 charecters");
+                        }
+                    }
+                }
+            },
+        },
+        {
+            freezeTableName: true,
+            hooks:{
+                beforeCreate: (record) =>{
+                    const salt = 10;    //Store in dotENV file?
+                    record.password = bcrypt.getEncryptedPassword(record.password, salt);
+                },
+                afterCreate:(record) =>{
+                    delete record.dataValues.password;
+                },
+                afterUpdate:(record) =>{
+                    delete record.dataValues.password;
+                }
+            }
+        });
+    }    
 }
-
-
-
--- utils/authUtil.js
+++ utils/authUtil.js
@@ -1,8 +1,11 @@
 const jwt = require("jsonwebtoken");
+const env = require('dotenv').config();
+
+
 module.exports = {
- generateToken: (data) => {
+ generateToken: (email) => {
   try {
-   const token = jwt.sign(data, 'jwtSecretc-chnage-this')
+   const token = jwt.sign(email, process.env.SECRET_KEY);
    return token;
   } catch (err) {
    return err;
